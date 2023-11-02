89a875cf43ddba1159e7319c3251a61a0c763c2a
Salvador De la Tijera <106124460+SalvadorDelaTijera@users.noreply.github.com>
2023-11-02 11:13:08 -0600

    arme la gallery como muestra y subi fotos de la gallery a la carpeta public

--- src/components/galery/Galery.jsx
+++ src/components/galery/Galery.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import './Galery.css'
 
 const Galery = () => {
+
   return (
-	<div>Gallery</div>
+	<div id='galleyConteiner'>
+    <div className='photos'><img src="/public/gallery/IMG_20231028_104609312_HDR (1) 1.png" alt="" /></div>
+    <div className='photos'><img src="/public/gallery/IMG_20230819_105843043_HDR 1.png" alt="" /></div>
+    <div className='photos'><img src="/public/gallery/IMG_20231028_104734610_HDR 4.png" alt="" /></div>
+    <div className='photos'><img src="/public/gallery/IMG_20231028_105052905_HDR.png" alt="" /></div>
+    <div className='photos'><img src="/public/gallery/IMG_20230719_112454472_HDR 1.png" alt="" /></div>
+  </div>
   )
 }
 
 export default Galery
+/*
+
+
+<div className='photos'><img src="/public/gallery/IMG_20230819_105928844_HDR.png" alt="" /></div>
+*/
