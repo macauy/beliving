4ddbcf8026f6bef4d7eb88570e988ee5353e0500
macauy <maca.rabosto@gmail.com>
2023-11-01 21:05:45 -0300

    elimino carpetas que no iban

--- public/icons/IconPhoneNumber/index.js
+++
@@ -1,1 +0,0 @@
-export { IconPhoneNumber } from "./IconPhoneNumber";
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,21 @@
+import "./Home.css";
+
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<main>
+			<section className="home-main">
+				<div className="home-logo-container">
+					<img className="main-container-logo" src="/logo.svg" alt="" />
+				</div>
+			</section>
+
+			<p>
+				Más que un servicio de alquiler de muebles para eventos, somos creadores
+				de experiencias únicas. Ofrecemos una amplia gama de livings y muebles
+				versátiles para tu evento.
+			</p>
+		</main>
+	);
 };
 
 export default Home;
