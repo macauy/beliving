d9fda4fed71cb7c2950562f83cfd48b92d707be7
macauy <maca.rabosto@gmail.com>
2023-11-12 21:18:39 -0300

    home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,21 +5,23 @@ import Galery from "../galery/Galery";
 const Home = () => {
 	return (
 		<main>
-			<section className="home-main">
-				<div className="home-logo-container">
-					<img className="home-logo" src="/logo.svg" alt="Logo" />
-				</div>
-			</section>
-			<section className="home-description">
-				<p>
-					Más que un <b>servicio de alquiler de muebles para eventos,</b> somos
-					creadores de experiencias únicas. Ofrecemos una amplia gama de livings
-					y muebles versátiles para tu evento.
-				</p>
-			</section>
-			<section className="home-button-container">
-				<Button type="default"></Button>
-			</section>
+			<div className="hero">
+				<section className="home-main">
+					<div className="home-logo-container">
+						<img className="home-logo" src="/logo.svg" alt="Logo" />
+					</div>
+				</section>
+				<section className="home-description">
+					<p>
+						Más que un <b>servicio de alquiler de muebles para eventos,</b>{" "}
+						somos creadores de experiencias únicas. Ofrecemos una amplia gama de
+						livings y muebles versátiles para tu evento.
+					</p>
+				</section>
+				<section className="home-button-container">
+					<Button type="default"></Button>
+				</section>
+			</div>
 			<section>
 				<Galery></Galery>
 			</section>
