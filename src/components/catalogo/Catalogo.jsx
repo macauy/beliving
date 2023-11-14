58a7d07c4d493fd8c8477530b5adf62768be534e
macauy <maca.rabosto@gmail.com>
2023-11-14 13:35:52 -0300

    cards y catalogo

--- src/components/catalogo/Catalogo.jsx
+++ src/components/catalogo/Catalogo.jsx
@@ -3,12 +3,27 @@ import Card from "../card/Card";
 
 const Catalogo = () => {
 	return (
-		<section className="catalogo">
-			<Card />
-			<Card />
-			<Card />
-			<Card />
-		</section>
+		<div className="catalogo">
+			<h1 className="catalogo-title">Conocé nuestras opciones</h1>
+			<section className="catalogo-livings">
+				<h2 className="catalogo-subtitle">LIVINGS</h2>
+				<div className="cards-container">
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+				</div>
+			</section>
+			<section className="catalogo-otros">
+				<h2 className="catalogo-subtitle">OTROS PRODUCTOS</h2>
+				<div className="cards-container">
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+				</div>
+			</section>
+		</div>
 	);
 };
 
