8856919b8853bab9790f26f583fda0614c59f24a
Salvador De la Tijera <106124460+SalvadorDelaTijera@users.noreply.github.com>
2023-11-29 01:25:09 -0600

    validación de datos de contacto

--- src/components/contacto/Contacto.jsx
+++ src/components/contacto/Contacto.jsx
@@ -1,4 +1,86 @@
-import { useState } from "react";
+//mport { useState } from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup"; //siempre se importa asi ---para mprotar todo (*) de la libreria yup y dale un nombre Yup
+import './Contacto.css'
+import styled from "@emotion/styled";
+
+
+const Contacto = () => {
+
+  const{handleSubmit, handleChange, errors}= useFormik({
+  
+    initialValues:{
+      nombre: "",
+      email: "",
+      telefono: "",
+      mensaje: ""
+    },
+    onSubmit: (data)=>{console.log(data)},
+//?----esquema de validacion Yup-------
+    validationSchema: Yup.object({ // se agregan todos los inputs a validar
+        nombre: Yup.string().required("Campo obligatorio").min(3, "Debe tener al menos 3 caractares"),
+        email: Yup.string().email("No es un email valido").required("Campo obligatorio"),
+        telefono: Yup.number().typeError("No es un número valido").required("Campo obligatorio").min(9, "Debe tener al menos 10 números"),
+        mensaje: Yup.string().required("Campo obligatorio").min(15, "Debe de tener al menos 15 caracteres")
+    }),
+   
+    validateOnChange:false,
+  });
+//console.log({errors});
+
+  return (
+    <div className="conteiner-contacto">
+        <div className='tituleConteiner'>Contacto</div>
+
+        <div className='mensajes-texto'>
+          <p>Completá el siguiente formulario y empezá a planificar tu próximo evento.</p>
+        </div>
+
+{/* ------------------------------------ FORMULARIO ----------------------------------------*/}
+        
+        <form className='formulario'  onSubmit={handleSubmit}>
+
+          <label htmlFor="nombre"><b>Nombre*</b></label>
+          <input type="text" name="nombre" onChange={handleChange} placeholder='Ingresá tu nombre' />
+            <span className="span">{errors.nombre}</span>
+          <label htmlFor="email"><b>Correo electronico*</b></label>
+          <input type="text"  name="email"  onChange={handleChange} placeholder='Ingresá tu e-mail'/>
+            <span className="span">{errors.email}</span>
+          <label htmlFor="telefono"><b>Telefono*</b></label>
+          <input type="text" name="telefono" onChange={handleChange} placeholder='Ingresá tu teléfono (o WhatsApp)'/>
+            <span className="span" >{errors.telefono}</span>
+
+{/* ------------------------------------ INSTRUCCIONES PARA MANDAR MENSAJE ----------------------------------------*/}
+         
+          <ul className='mensajes-texto'>
+            <p><b>En el siguiente espacio nos gustaría que nos cuentes: </b></p>
+              <li> - qué mobiliario te interesa.</li>
+              <li> - qué tipo de evento querés realizar.</li>
+              <li> - cuántas personas son. </li>
+              <li> - cuándo y en qué lugar se realizará (salón de evento, casa particular, SUM, etc.)</li>
+          </ul>
+{/* ------------------------------------ CAJA DE TEXTO DEL MENSAJE ----------------------------------------*/}
+
+          <label htmlFor="mensaje"><b>Mensaje*</b></label>
+          <textarea className='mensaje-conteiner' name="mensaje" onChange={handleChange} placeholder='Contanos sobre tu evento'/>
+              <span className="span">{errors.mensaje}</span>
+
+{/* ------------------------------------ BOTON ENVIAR ----------------------------------------*/}
+            
+          <button className="contacto-button" type='submit'> 
+            <div className="contacto-button-text">ENVIAR</div>
+          </button>
+      </form>
+
+      <div className='mensajes-texto'>
+        <p>¡Nos contactaremos con vos a la brevedad! Recordá que también podés contactarnos por <b>WhatsApp.</b></p>
+      </div>
+    </div>
+  )
+}
+
+ /*
+ import { useState } from "react";
 import './Contacto.css'
 
 
@@ -23,9 +105,7 @@ const Contacto = () => {
       //setError("Por Favor completa todos los campos );
       alert("Por Favor completa todos los campos correctamente")
     }
-    if (telefono ) {
-      
-    }
+    
 
    //! PARA SETEAR 
    setNombre("");
@@ -63,7 +143,7 @@ const Contacto = () => {
           </ul>
 
           <label htmlFor="mensaje"><b>Mensaje*</b></label>
-          <textarea className='mensaje-conteiner 'value={mensaje} onChange={(e)=>setMensaje(e.target.value)} placeholder='Contanos sobre tu evento'/>
+          <textarea className='mensaje-conteiner 'value={mensaje} onChange={} placeholder='Contanos sobre tu evento'/>
         
           <button className="contacto-button" type='submit'> 
             <div className="contacto-button-text">ENVIAR</div>
@@ -81,8 +161,11 @@ const Contacto = () => {
     </div>
   )
 }
+ 
+ 
+ 
 
- /*para crrar un nuevo documento por cada usuario en firebase 
+ para crrar un nuevo documento por cada usuario en firebase 
     addDoc agrega un docuemnto nuevo a la coleccion.
 
     addDoc(collection(db, "nombre de la coleccion"),{
@@ -91,5 +174,7 @@ const Contacto = () => {
       telefono: telefono,
       mensaje: mensaje;
     })
-    */ 
+
+*/ 
+
 export default Contacto
