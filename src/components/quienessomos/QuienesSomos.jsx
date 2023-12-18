58ab325897fe99d9707d1ed3a3ccb9a527ab5510
macauy <maca.rabosto@gmail.com>
2023-12-18 18:07:08 -0300

    quienes somos

--- src/components/quienessomos/QuienesSomos.jsx
+++ src/components/quienessomos/QuienesSomos.jsx
@@ -2,30 +2,27 @@ import "./QuienesSomos.css";
 
 const QuienesSomos = () => {
 	return (
-		<div className="section-quienes-conteiner">
-			<div className="quienes-img-conteiner">
-				{/* <img
-					className="img-quienessomos"
-					src="../../../public/quienes somos.jpg"
-					alt=""
-				/> */}
-
+		<div className="section-quienes-container">
+			<div className="quienes-img-container">
 				<img className="img-quienessomos" src="/somoscollage.webp" alt="" />
 			</div>
 
 			<div className="hero-mobile">
 				<div className="quienes-somos-image-container-mobile"></div>
 			</div>
-			<div className="quienes-texto-conteiner">
-				<h3 className="quienes-antetitulo title">Quiénes somos</h3>
-				<p className="quienes-texto">
-					<b>¡Bienvenidos!</b> Somos Paola y Martin, comenzamos con Be Living
-					hace poco mas de tres años, nos especializamos en el alquiler de
-					mobiliario para fiestas y eventos,trabajamos muy cerca de nuestros
-					clientes para entender sus necesidades y ofrecer soluciones prácticas,
-					con el objetivo de superar sus expectativas y asi lograr su
-					satisfacción.
-				</p>
+
+			<div className="quienes-texto-container">
+				<h3 className="quienes-title title">Quiénes somos</h3>
+				<div className="quienes-texto">
+					<p>
+						<b>¡Bienvenidos!</b> Somos Paola y Martin, comenzamos con Be Living
+						hace poco mas de tres años, nos especializamos en el alquiler de
+						mobiliario para fiestas y eventos,trabajamos muy cerca de nuestros
+						clientes para entender sus necesidades y ofrecer soluciones
+						prácticas, con el objetivo de superar sus expectativas y asi lograr
+						su satisfacción.
+					</p>
+				</div>
 			</div>
 		</div>
 	);
