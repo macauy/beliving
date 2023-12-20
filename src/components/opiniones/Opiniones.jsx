af715374b3ff8c71383d33f49fb023b1529e5fd9
Salvador De la Tijera <106124460+SalvadorDelaTijera@users.noreply.github.com>
2023-12-20 09:44:32 -0600

    media queires

--- src/components/opiniones/Opiniones.jsx
+++ src/components/opiniones/Opiniones.jsx
@@ -6,7 +6,7 @@ import "./Opiniones.css";
 export const Opiniones = () => {
 	return (
 		<div className="opiniones">
-			<div className="opiniones-titular-conteiner">
+			<div className="title-conteiner">
 				<h2 className="title">Qué piensan nuestros clientes</h2>
 			</div>
 			<div className="frame">
