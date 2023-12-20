a97ae7a3b1470ce93d6f5bba5420464d82934f18
macauy <maca.rabosto@gmail.com>
2023-12-20 11:54:23 -0300

    slider

--- src/components/opinionesslidermobile/OpinionesSlidermobile.jsx
+++ src/components/opinionesslidermobile/OpinionesSlidermobile.jsx
@@ -1,45 +1,45 @@
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
-import '../opinionineseach/opinionmelisa/OpinionMelisa'
-import OpininonMelisa from '../opinionineseach/opinionmelisa/OpinionMelisa'
-import '../opinionineseach/opinionmaria/OpinionMaria'
-import OpinionMaria from '../opinionineseach/opinionmaria/OpinionMaria';
-import '../opinionineseach/opinionemilse/OpinionEmilse'
-import OpinionEmilse from '../opinionineseach/opinionemilse/OpinionEmilse';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "../opinionineseach/opinionmelisa/OpinionMelisa";
+import OpininonMelisa from "../opinionineseach/opinionmelisa/OpinionMelisa";
+import "../opinionineseach/opinionmaria/OpinionMaria";
+import OpinionMaria from "../opinionineseach/opinionmaria/OpinionMaria";
+import "../opinionineseach/opinionemilse/OpinionEmilse";
+import OpinionEmilse from "../opinionineseach/opinionemilse/OpinionEmilse";
 import "swiper/css";
-import './OpinionesSlidermobile.css'
+import "./OpinionesSlidermobile.css";
 
 const OpinionesSlidermobile = () => {
-  return (
-    <div className='slider-opinions-conteiner'>
-      
-      <Swiper 
-        slidesPerView={1}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-      
-        modules={[Pagination]}
-        className="mySwiper-slider-opinions"
-      >
-        <SwiperSlide className='slide-opinion'> 
-              <OpininonMelisa/>
-        </SwiperSlide>
-        
-        <SwiperSlide className='slide-opinion'>
-              <OpinionMaria/>
-        </SwiperSlide>
+	return (
+		<div className="slider-opinions-conteiner">
+			<Swiper
+				slidesPerView={1}
+				spaceBetween={30}
+				pagination={{
+					clickable: true,
+				}}
+				style={{
+					"--swiper-navigation-color": "#fff",
+					"--swiper-pagination-color": "var(--primary)",
+				}}
+				modules={[Pagination]}
+				className="mySwiper-slider-opinions"
+			>
+				<SwiperSlide className="slide-opinion">
+					<OpininonMelisa />
+				</SwiperSlide>
 
-        <SwiperSlide className='slide-opinion'>
-              <OpinionEmilse/>
-        </SwiperSlide>
-        
-      </Swiper>
+				<SwiperSlide className="slide-opinion">
+					<OpinionMaria />
+				</SwiperSlide>
 
-    </div>
-  )
-}
+				<SwiperSlide className="slide-opinion">
+					<OpinionEmilse />
+				</SwiperSlide>
+			</Swiper>
+		</div>
+	);
+};
 
-export default OpinionesSlidermobile
+export default OpinionesSlidermobile;
